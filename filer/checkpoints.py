91f2e0b4de60fccbc2dd61324652b0f4ec2575e6
aka7774 <aka7774@users.noreply.github.com>
2022-12-11 17:42:49 +0900

    Fix Checkpoint Active List

--- filer/checkpoints.py
+++ filer/checkpoints.py
@@ -9,18 +9,20 @@ def load_active_dir():
     for c in sd_models.checkpoints_list.values():
         return os.path.dirname(c.filename)
 
-def list_active():
+def list(dir):
     data = filer_models.load_comment('checkpoints')
     rs = []
-    for c in sd_models.checkpoints_list.values():
-        filename = os.path.basename(c.filename)
+    for filename in os.listdir(dir):
+        if not filename.endswith('.ckpt') and not filename.endswith('.safetensors') and not filename.endswith('.vae.pt'):
+            continue
+
         d = data[filename] if filename in data else {}
 
         r = {}
         r['title'] = filename
         r['filename'] = filename
-        r['filepath'] = c.filename
-        r['hash'] = c.hash
+        r['filepath'] = os.path.join(dir, filename)
+        r['hash'] = sd_models.model_hash(r['filepath'])
         r['sha256_path'] = r['filepath'] + '.sha256'
         r['sha256'] = pathlib.Path(r['sha256_path']).read_text()[:16] if os.path.exists(r['sha256_path']) else ''
         r['vae_path'] = os.path.splitext(r['filepath'])[0] + '.vae.pt'
@@ -34,36 +36,14 @@ def list_active():
 
     return rs
 
+def list_active():
+    return list(load_active_dir())
+
 def list_backup():
     backup_dir = filer_models.load_backup_dir('checkpoints')
     if not backup_dir or not os.path.exists(backup_dir):
         return []
-
-    data = filer_models.load_comment('checkpoints')
-    rs = []
-    for filename in os.listdir(backup_dir):
-        if not filename.endswith('.ckpt') and not filename.endswith('.safetensor') and not filename.endswith('.vae.pt'):
-            continue
-
-        d = data[filename] if filename in data else {}
-
-        r = {}
-        r['title'] = filename
-        r['filename'] = filename
-        r['filepath'] = os.path.join(backup_dir, filename)
-        r['hash'] = sd_models.model_hash(r['filepath'])
-        r['sha256_path'] = r['filepath'] + '.sha256'
-        r['sha256'] = pathlib.Path(r['sha256_path']).read_text()[:8]+'...' if os.path.exists(r['sha256_path']) else ''
-        r['vae_path'] = os.path.splitext(r['filepath'])[0] + '.vae.pt'
-        r['vae'] = 'Y' if os.path.exists(r['vae_path']) else ''
-        r['yaml_path'] = os.path.splitext(r['filepath'])[0] + '.yaml'
-        r['yaml'] = 'Y' if os.path.exists(r['yaml_path']) else ''
-        r['genre'] = d['genre'] if 'genre' in d else ''
-        r['comment'] = d['comment'] if 'comment' in d else ''
-
-        rs.append(r)
-
-    return rs
+    return list(backup_dir)
 
 def make_yaml(filenames, list):
     y = {}
